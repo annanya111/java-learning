351f3d9aa6e91717c7abf24a5385b0e7ecb58b03
Annanya Tiwari <annanyatiwari2004@gmail.com>
2026-01-10 21:47:57 +0530

    Add files via upload
    Day 1: Variables and printing in Java

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args) {
+        int age = 21;
+        double cgpa = 8.2;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Annanya";
+    System.out.println("Name:" +name);
+    System.out.println("Cgpa:" +cgpa);
+    System.out.println("Grade:" +grade);
+    System.out.println("Age:" +age);
+    System.out.println("Student:" +isStudent);
+    }
+}
