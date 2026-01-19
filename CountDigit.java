1ffebe79ee119ba6f964842263c01a631d0c436d
Annanya Tiwari <annanyatiwari2004@gmail.com>
2026-01-19 23:25:23 +0530

    Add files via upload
    Day 8 – Count digits using while loop

---
+++ CountDigit.java
@@ -0,0 +1,18 @@
+public class CountDigit {
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int count = 0;
+
+        if (number == 0) {
+            count = 1;
+        } else {
+            while (number != 0) {
+                number = number / 10;
+                count++;
+            }
+        }
+
+        System.out.println("Number of digits = " + count);
+    }
+}
