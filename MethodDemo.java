2c32baad409609aecc5abc811fdbfaa456227bc3
Annanya Tiwari <annanyatiwari2004@gmail.com>
2026-02-01 00:04:40 +0530

    Add files via upload
    Day 21: Introduction to methods in Java

---
+++ MethodDemo.java
@@ -0,0 +1,14 @@
+public class MethodDemo {
+
+    // method to add two numbers
+    static void addNumbers(int a, int b) {
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+
+    public static void main(String[] args) {
+
+        addNumbers(10, 20);
+        addNumbers(5, 7);
+    }
+}
