5bb014630e696d1e65fc55cefb6b976f4586e345
Annanya Tiwari <annanyatiwari2004@gmail.com>
2026-02-10 12:17:15 +0530

    Add files via upload
    Day 29 – Java OOPS (Class & Object)
    Learned the basics of Object-Oriented Programming.
    Implemented simple programs using class, object, and methods.

---
+++ Calc.java
@@ -0,0 +1,16 @@
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+}
+
+public class Calc {
+    public static void main(String[] args) {
+
+        Calculator c = new Calculator();
+        int result = c.add(10, 20);
+
+        System.out.println("Sum: " + result);
+    }
+}
